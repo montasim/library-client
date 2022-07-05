41366239d375ea10ae290753179506861e9958c8
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-06 01:40:07 +0600

    sidebar size increased

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,16 +6,16 @@ import NavLinkComponents from './NavLinkComponents';
 
 const Sidebar = () => {
     return (
-        <div class="drawer drawer-mobile shadow-xl">
+        <div class="drawer drawer-mobile">
             <input id="dashboard" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content flex flex-col">
+            <div class="drawer-content flex flex-col mt-10">
                 <Outlet />
             </div>
             <div class="drawer-side">
                 <label for="dashboard" class="drawer-overlay"></label>
-                <div class="flex flex-col justify-between">
+                <div class="flex flex-col justify-between w-48">
                     <div class="p-2">
-                        <nav class="flex flex-col mt-6 space-y-1 list-none">
+                        <nav class="flex flex-col mt-3 md:mt-4 lg:mt-6 space-y-1 list-none text-lg">
                             <NavLinkComponents to={'/students'} icon={<FaUserFriends className='text-lg' />} name={'Students'} />
 
                             <NavLinkComponents to={'books'} icon={<ImBooks className='text-lg' />} name={'Books'} />
