89588b899e2e8fb5e5585b6f9a2613b025d47dfd
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-06 01:52:47 +0600

    edit & view btn

---
+++ src/components/ViewButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { GrView } from 'react-icons/gr';
+
+const ViewButton = () => {
+    return (
+        <button
+            className="z-10 block p-2 text-blue-700 transition-all bg-blue-100 border-2 border-white rounded-full active:bg-blue-50 hover:scale-110 focus:outline-none focus:ring"
+            type="button"
+        >
+            <GrView />
+        </button>
+    );
+};
+
+export default ViewButton;
