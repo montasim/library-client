0c8345d6f716fa55334690759f7cdaabc2f4313e
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-06 20:11:21 +0600

    edit student

--- src/components/EditButton.js
+++ src/components/EditButton.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { BiEdit } from 'react-icons/bi';
 
-const EditButton = () => {
+const EditButton = ({ modalId }) => {
     return (
-        <button
-            className="z-10 block p-2 text-blue-700 transition-all bg-blue-100 border-2 border-white rounded-full active:bg-blue-50 hover:scale-110 focus:outline-none focus:ring"
-            type="button"
-        >
-            <BiEdit />
-        </button>
+        <label for={`${modalId}`} class="btn btn-circle bg-indigo-200 text-indigo-500 border-none btn-sm modal-button">
+            <BiEdit className='text-lg' />
+        </label>
     );
 };
 
--- src/pages/Students/StudentRow.js
+++ src/pages/Students/StudentRow.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import DeleteButton from '../../components/DeleteButton';
 import EditButton from '../../components/EditButton';
+import Input from '../../components/Input';
+import ModalCloseButton from '../../components/ModalCloseButton';
+import NewButton from '../../components/NewButton';
 import ViewButton from '../../components/ViewButton';
 
 const StudentRow = () => {
     const tableRowsData = ['1', 'Montasim', 'Mamun'];
 
     return (
-        <tr>
-            {
-                tableRowsData?.map(tableRowData =>
-                    <td>{tableRowData}</td>)
-            }
-            <td className='flex gap-x-4'>
-                <EditButton />
-                <DeleteButton />
-            </td>
-        </tr>
+        <>
+            <input type="checkbox" id="edit-student" class="modal-toggle" />
+            <label for="edit-student" class="modal cursor-pointer">
+                <label class="modal-box w-2/5 max-w-4xl relative" for="">
+                    <ModalCloseButton modalId={'edit-student'} />
+
+                    <h2 className='text-lg text-center font-bold'>Edit a Student</h2>
+
+                    <div className='flex justify-between place-content-center mt-6'>
+                        <Input title={'First Name'} />
+                        <Input title={'Last Name'} />
+                    </div>
+                </label>
+            </label>
+            <tr>
+                {
+                    tableRowsData?.map(tableRowData =>
+                        <td>{tableRowData}</td>)
+                }
+                <td className='flex gap-x-4'>
+                    <EditButton modalId={'edit-student'} />
+                    <DeleteButton />
+                </td>
+            </tr>
+        </>
     );
 };
 
