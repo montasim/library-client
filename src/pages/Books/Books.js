e142a7bdfbc1a045881c3b55336acbfbf25a45f1
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-06 19:44:52 +0600

    add new book

--- src/pages/Books/Books.js
+++ src/pages/Books/Books.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Input from '../../components/Input';
+import ModalCloseButton from '../../components/ModalCloseButton';
 import NewButton from '../../components/NewButton';
 import RefreshButton from '../../components/RefreshButton';
 import BookRow from './BookRow';
@@ -15,9 +17,23 @@ const Students = () => {
     return (
         <section className='p-1'>
             <div className="flex justify-between mb-6">
-                <NewButton modalId={'create-new-product'} />
+                <NewButton modalId={'add-new-book'} />
                 <RefreshButton />
             </div>
+
+            <input type="checkbox" id="add-new-book" class="modal-toggle" />
+            <label for="add-new-book" class="modal cursor-pointer">
+                <label class="modal-box w-2/5 max-w-4xl relative" for="">
+                    <ModalCloseButton modalId={'add-new-book'} />
+
+                    <h2 className='text-lg text-center font-bold'>Add a New Book</h2>
+
+                    <div className='flex justify-between place-content-center mt-6'>
+                        <Input title={'Book Name'} />
+                        <Input title={'Author'} />
+                    </div>
+                </label>
+            </label>
 
             <table class="table table-zebra table-compact">
                 <thead>
